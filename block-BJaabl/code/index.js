8489a14430ba12befb3c182adcc3c85d2bd6424a
vandana <vandana123annusah@gmail.com>
2021-11-07 14:39:06 +0530

    completd function assignment block 4

--- block-BJaabl/code/index.js
+++ block-BJaabl/code/index.js
@@ -2,6 +2,14 @@
 1. Create a function named `sayHello` that that accepts a parameter `name` and alert `Hello [name]!`.
 */
 
+
+function sayHello () {
+  let name = "vandana"
+  alert(`Hello ${name}`);
+}
+sayHello ();
+
+
 /*
 2. Create a function named `getFullName` that accepts two parameter `firstName` and `lastName` and returns
 fullName.
@@ -10,6 +18,14 @@ Example:
   getFullName("John", "Snow"); // "John Snow"
   getFullName("Nelson", "Mandela"); // "Nelson Mandela"
 */
+
+function getFullName (firstName, lastName) {
+  
+  return `${firstName} ${lastName}`;
+}
+let name1 = getFullName ("John", "Snow");
+let name2 = getFullName ("Nelson", "Mandela");
+
 
 /*
 3. Create a function named `addTwoNumbers` that accepts two numbers i.e `firstNum` and `secondNum` and returns
@@ -20,6 +36,19 @@ addTwoNumbers(10, 22); // 32
 addTwoNumbers(20, 32); // 32
 addTwoNumbers(10, "100"); // Alert Enter Valid Input
 */
+
+
+function addTwoNumbers (firstNum, secondNum) {
+  if(typeof firstNum === 'number' && typeof secondNum === 'number'){
+    return firstNum + secondNum;
+  } else{
+    alert("Enter valid input");
+  }
+} 
+let num1 = addTwoNumbers (10, 22);
+let num2 = addTwoNumbers (20, 32);
+let num3 = addTwoNumbers(10, "100");
+
 
 /*
 4. Create a function named `calc` which accepts three parameter `numA`, `numB` and `operation`. Operation
@@ -33,6 +62,22 @@ calc(20, 10, 'sub'); // 10
 calc(20, 10, 'mul'); // 200
 */
 
+function calc (numA, numB, operator) {
+  if(typeof numA === 'number' &&  typeof numB === 'number' && operator === 'add'){
+    return numA + numB ;
+  } else if(typeof numA === 'number' &&  typeof numB === 'number' && operator === 'sub'){
+    return numA - numB ;
+  } else if(typeof numA === 'number' &&  typeof numB === 'number' && operator === 'mul'){
+    return numA * numB ;
+  } else {
+    alert("Enter Valid Input");
+  }
+}
+
+
+let numA = calc(12, 12 ,'add');
+let numB = calc(20, 10, 'sub');
+let numc = calc(10, 10, 'mul');
 
 /*
 5. Create a function named `isLeapYear` that accepts a number data type and return `true` or `false` based
@@ -42,7 +87,33 @@ isLeapYear(2000); // true
 isLeapYear(2001); // false
 */
 
+function isLeapYear (year) {
+  if(year % 4 === 0 && year % 400 === 0) {
+    return true;
+  } else if(year % 100 === 0 ) {
+    return false;
+  } else {
+    return false;
+  }
+}
+
+let year1 = isLeapYear(2000);
+let year2 = isLeapYear(2001);
+
 
 /*
 6. Create a function named `getFactorial` that accepts a number and return the factorial of the number.
 */
+
+   
+function getFactorial(number) {
+
+  let factorial = 1;
+  for(let i=1; i<= number; i++){
+     factorial = factorial *i;
+  }
+  return(`The Factorial of ${number} is ${factorial}`);
+}
+
+let number = getFactorial(5);
+
